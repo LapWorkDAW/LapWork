c84f821975f47f51fcadc6f24c07ec4060bdb63b
Mireia <mireiacolomerr@gmail.com>
2018-12-03 12:01:23 +0100

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.css']
 })
 export class AppComponent {
   title = 'LapWork';
