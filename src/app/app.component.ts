0df63d58c6223bcb9677a9ca5481d41f9b223b50
Mireia <mireiacolomerr@gmail.com>
2018-12-13 11:41:58 +0100

    try to push correctly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.css']
 })
 export class AppComponent {
-  
+  projects = [{
+    idProject: "1",
+    creator: "Mireia Colomer",
+    name: "Marver Character Creation",
+    desc: "This project consists in the creation for a movie.",
+    type: "Tech",
+    dateStart: "10/12/2018",
+    dateFinish: "29/06/2018",
+    img: "",
+    projectStatus: ""
+    /* likes:"" */
+  }, {
+    idProject: "2",
+    creator: "Mireia Colomer",
+    name: "Framework Creation",
+    desc: "This project consists in the creation for our webpage framework",
+    type: "Tech",
+    dateStart: "10/12/2018",
+    dateFinish: "29/06/2018",
+    img: "",
+    projectStatus: ""
+    /* likes:"" */
+  }];
+  project = {
+    idProject: "",
+    creator: "",
+    name: "",
+    desc: "",
+    type: "",
+    dateStart: "",
+    dateFinish: "",
+    img: "",
+    projectStatus: ""
+    /* likes:"" */
+  };
+
+
 }
